2db0d80131b8f8d9e77edd57b10c65745e5143eb
Alex Norton <alexnorton@fastmail.com>
2018-07-16 22:49:10 +0100

    Add basic routes

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
+
+const BASE_DIR = "/Volumes/Irwell/Music/iTunes/iTunes Music/Music";
 
 const PORT = 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+const withoutThe = input => input.match(/^(?:The )?(.+)$/)[1];
+
+app.get("/browse", function(req, res) {
+  const dir = path.join(BASE_DIR, req.query.path);
+
+  fs.readdir(dir, function(err, files) {
+    res.json(files.sort((a, b) => withoutThe(a).localeCompare(withoutThe(b))));
+  });
+});
+
+app.get("/get", function(req, res) {
+  var path = BASE_DIR + (req.query.path || "") + req.query.file;
+
+  res.sendFile(path);
 });
 
 app.listen(PORT, () => {
