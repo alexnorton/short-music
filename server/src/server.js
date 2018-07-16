82f614bc64cbd658afcba6e696d7749fb162bb70
Alex Norton <alexnorton@fastmail.com>
2018-07-16 22:05:44 +0100

    Initial commit

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
