f612d18faedcbafc9e4c0ec2cf9a1c84b3716e54
Alex Norton <alexnorton@fastmail.com>
2018-07-17 15:21:50 +0100

    Use dotenv to load config from file

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
+require("dotenv").config();
+
 const app = express();
 
-const BASE_DIR = "/Volumes/Irwell/Music/iTunes/iTunes Music/Music";
-
-const PORT = 3001;
+const BASE_DIR = process.env.BASE_DIR;
+const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -15,7 +16,7 @@ app.get("/", (req, res) => {
 const withoutThe = input => input.match(/^(?:The )?(.+)$/)[1];
 
 app.get("/browse", function(req, res) {
-  const dir = path.join(BASE_DIR, req.query.path);
+  const dir = path.join(BASE_DIR, req.query.path || "");
 
   fs.readdir(dir, function(err, files) {
     res.json(files.sort((a, b) => withoutThe(a).localeCompare(withoutThe(b))));
@@ -23,9 +24,9 @@ app.get("/browse", function(req, res) {
 });
 
 app.get("/get", function(req, res) {
-  var path = BASE_DIR + (req.query.path || "") + req.query.file;
+  var filePath = path.join(BASE_DIR, req.query.path, req.query.file);
 
-  res.sendFile(path);
+  res.sendFile(filePath);
 });
 
 app.listen(PORT, () => {
