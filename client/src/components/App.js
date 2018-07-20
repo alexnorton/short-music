8f905d40b7dcd1efa7df55add8cf44ceed4f1b1c
Alex Norton <alexnorton@fastmail.com>
2018-07-20 07:47:13 -0400

    Use BrowserRouter instead of HashRouter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Browser from "../containers/Browser";
 import Controls from "../containers/Controls";
@@ -8,12 +8,12 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <HashRouter>
+        <BrowserRouter>
           <Switch>
             <Route path="/:path+" component={Browser} />
             <Route path="/" exact component={Browser} />
           </Switch>
-        </HashRouter>
+        </BrowserRouter>
         <Controls />
       </div>
     );
