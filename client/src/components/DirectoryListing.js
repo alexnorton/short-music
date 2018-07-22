92f8a687599682ef1188615dd6d8f54f4ce904e4
Alex Norton <alexnorton@fastmail.com>
2018-07-22 17:57:49 -0400

    Improve browser appearance

--- client/src/components/DirectoryListing.js
+++ client/src/components/DirectoryListing.js
@@ -4,6 +4,40 @@ import styled from "styled-components";
 
 import removeArticle from "../helpers/removeArticle";
 
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto 30px;
+`;
+
+const DirectoryHeading = styled.h2`
+  font-size: 32px;
+`;
+
+const ContentTypeHeading = styled.h3`
+  font-size: 24px;
+`;
+
+const ContentList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const ContentItem = styled.li`
+  line-height: 1.4em;
+`;
+
+const ContentTypeListing = ({ items, heading, renderItem }) => (
+  <Fragment>
+    <ContentTypeHeading>{heading}</ContentTypeHeading>
+    <ContentList>
+      {items
+        .sort((a, b) => removeArticle(a).localeCompare(removeArticle(b)))
+        .map(item => <ContentItem key={item}>{renderItem(item)}</ContentItem>)}
+    </ContentList>
+  </Fragment>
+);
+
 const FileButton = styled.button`
   background: none !important;
   color: #2980b9;
@@ -20,51 +54,41 @@ const FileButton = styled.button`
 `;
 
 const DirectoryListing = ({ data, path, onSelectFile }) => (
-  <div>
-    <h2>{path && path.join(" / ")}</h2>
+  <Container>
+    <DirectoryHeading>
+      {path && path.length > 0 ? path.join(" / ") : "Home"}
+    </DirectoryHeading>
     {path.length > 0 && (
       <Link to={"/" + path.slice(0, path.length - 1).join("/")}>
         Up one level
       </Link>
     )}
     {data.directories.length > 0 && (
-      <Fragment>
-        <h3>Directories</h3>
-        <ul>
-          {data.directories
-            .sort((a, b) => removeArticle(a).localeCompare(removeArticle(b)))
-            .map(directory => (
-              <li key={directory}>
-                <Link to={"/" + [...path, directory].join("/")}>
-                  {directory}
-                </Link>
-              </li>
-            ))}
-        </ul>
-      </Fragment>
+      <ContentTypeListing
+        items={data.directories}
+        heading="Directories"
+        renderItem={directory => (
+          <Link to={"/" + [...path, directory].join("/")}>{directory}</Link>
+        )}
+      />
     )}
     {data.files.length > 0 && (
-      <Fragment>
-        <h3>Files</h3>
-        <ul>
-          {data.files
-            .sort((a, b) => removeArticle(a).localeCompare(removeArticle(b)))
-            .map(file => (
-              <li key={file}>
-                <FileButton
-                  onClick={e => {
-                    e.preventDefault();
-                    onSelectFile(path, file);
-                  }}
-                >
-                  {file}
-                </FileButton>
-              </li>
-            ))}
-        </ul>
-      </Fragment>
+      <ContentTypeListing
+        items={data.files}
+        heading="Files"
+        renderItem={file => (
+          <FileButton
+            onClick={e => {
+              e.preventDefault();
+              onSelectFile(path, file);
+            }}
+          >
+            {file}
+          </FileButton>
+        )}
+      />
     )}
-  </div>
+  </Container>
 );
 
 export default DirectoryListing;
