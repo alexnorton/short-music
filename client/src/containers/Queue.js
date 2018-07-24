f4db35c1c606b1a4b42a1bf99e2f94e6d985e033
Alex Norton <alexnorton@fastmail.com>
2018-07-23 21:22:29 -0400

    Show queue empty message

--- client/src/containers/Queue.js
+++ client/src/containers/Queue.js
@@ -2,30 +2,42 @@ import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 
+const StyledQueue = styled.div`
+  padding: 10px;
+`;
+
 const QueueList = styled.ul`
   margin: 0;
-  padding: 10px 10px 0;
+  padding: 0;
   list-style: none;
 `;
 
 const QueueItem = styled.li`
-  margin-bottom: 10px;
+  :not(:last-child) {
+    margin-bottom: 10px;
+  }
 `;
 
 const Queue = ({ queue }) => (
-  <QueueList>
-    {queue.map(path => {
-      const file = path[path.length - 1];
-      const directoryPath = path.slice(0, path.length - 1);
-      return (
-        <QueueItem key={path.join("/")}>
-          {file}
-          <br />
-          {directoryPath.join(" / ")}
-        </QueueItem>
-      );
-    })}
-  </QueueList>
+  <StyledQueue>
+    {queue.length > 0 ? (
+      <QueueList>
+        {queue.map(path => {
+          const file = path[path.length - 1];
+          const directoryPath = path.slice(0, path.length - 1);
+          return (
+            <QueueItem key={path.join("/")}>
+              {file}
+              <br />
+              {directoryPath.join(" / ")}
+            </QueueItem>
+          );
+        })}
+      </QueueList>
+    ) : (
+      "Queue empty"
+    )}
+  </StyledQueue>
 );
 
 const mapStateToProps = ({ player: { queue } }) => ({
