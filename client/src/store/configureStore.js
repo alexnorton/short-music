f589ac9ae814d79e41bd9794f82169fd94e7e063
Alex Norton <alexnorton@fastmail.com>
2018-07-25 16:36:46 -0400

    Don't send time update or progress actions to Redux dev tools

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -2,8 +2,13 @@ import { createStore, applyMiddleware, compose } from "redux";
 
 import rootReducer from "../reducers/rootReducer";
 import playerMiddleware from "../player/playerMiddleware";
+import { TIME_UPDATE, PROGRESS } from "../actions/player";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      actionsBlacklist: [TIME_UPDATE, PROGRESS],
+    })
+  : compose;
 
 const configureStore = player =>
   createStore(
