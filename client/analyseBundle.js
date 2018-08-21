cab0b7dab743110e3a1d048a9d8b44fa3d43edca
Alex Norton <alexnorton@fastmail.com>
2018-08-21 10:51:05 +0100

    Update analyse bundle script to work with TypeScript

--- client/analyseBundle.js
+++ client/analyseBundle.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = "production";
 
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const webpackConfigProd = require("react-scripts/config/webpack.config.prod");
+const webpackConfigProd = require("react-scripts-ts/config/webpack.config.prod");
 
 webpackConfigProd.plugins.push(
   new BundleAnalyzerPlugin({
@@ -10,4 +10,4 @@ webpackConfigProd.plugins.push(
   })
 );
 
-require("react-scripts/scripts/build");
+require("react-scripts-ts/scripts/build");
