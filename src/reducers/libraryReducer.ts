622e9f34ee00468330c71389f7d657e3fe714a8b
Alex Norton <alexnorton@fastmail.com>
2018-09-03 18:31:13 +0100

    Implement library reducer

--- src/reducers/libraryReducer.ts
+++ src/reducers/libraryReducer.ts
@@ -1,15 +1,52 @@
 import Directory from "../model/Directory";
-import { LibraryAction } from "../actions/library";
+import {
+  LibraryAction,
+  DIRECTORY_REQUEST,
+  DIRECTORY_FAILURE,
+  DIRECTORY_SUCCESS,
+} from "../actions/library";
 
 export interface LibraryState {
-  [path: string]: Directory;
+  [path: string]: {
+    contents?: Directory;
+    isFetching: boolean;
+    error?: string;
+  };
 }
 
 const libraryReducer = (
   state: LibraryState = {},
   action: LibraryAction
 ): LibraryState => {
-  return state;
+  switch (action.type) {
+    case DIRECTORY_REQUEST:
+      return {
+        ...state,
+        [action.path]: {
+          isFetching: true,
+        },
+      };
+    case DIRECTORY_FAILURE:
+      return {
+        ...state,
+        [action.path]: {
+          ...state[action.path],
+          isFetching: false,
+          error: action.error,
+        },
+      };
+    case DIRECTORY_SUCCESS:
+      return {
+        ...state,
+        [action.path]: {
+          ...state[action.path],
+          isFetching: false,
+          contents: action.directory,
+        },
+      };
+    default:
+      return state;
+  }
 };
 
 export default libraryReducer;
